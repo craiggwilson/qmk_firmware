f7bbddcf14b4f03e2280e020cb673a183c4f029a
craiggwilson <craiggwilson@gmail.com>
2021-05-08 08:56:08 -0500

    craiggwilson variant

---
+++ keyboards/massdrop/ctrl/keymaps/craiggwilson/keymap.h
@@ -0,0 +1,8 @@
+#include QMK_KEYBOARD_H
+
+enum layout_names {
+    _TL=0, // Typing Layout: The main keyboard layout for typing.
+    _FL,   // Function Layout: The function key activated layout with default functions and some added ones
+    _NL,   // Navigation Layout: brings the arrow keys to WASD and the mouse to UIOJKL
+};
+
---
+++ keyboards/massdrop/ctrl/keymaps/craiggwilson/rules.mk
@@ -0,0 +1,5 @@
+# This keymap requires Massdrop Configurator support
+OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
+
+MOUSEKEY_ENABLE = yes			# Enable mouse control keycodes. Increases firmware size.
+TAP_DANCE_ENABLE = no			# Enable tap dance keys
